f16f2e8667ff723e50093099cd65bb62a65b6bdf
Sam Westerman <sam.westerman@usaepay.com>
2021-07-10 21:27:31 -0700

    Added posix

---
+++ src/posix.c
@@ -0,0 +1,28 @@
+#ifdef __GNUC__
+struct _nothing_to_see_here;
+#else
+
+#include <string.h>
+#include <stdint.h>
+#include "shared.h"
+
+char *strdup(char *str) {
+	size_t length = strlen(str);
+
+	return memcpy(xmalloc(length), str, length);
+}
+
+char *strndup(char *str, size_t maxlen) {
+	size_t length = strlen(str);
+
+	if (length < maxlen)
+		return strdup(str);
+
+	char *result = xmalloc(maxlen);
+	memcpy(result, str, maxlen);
+	result[maxlen] = '\0';
+
+	return result;
+}
+
+#endif /* __GNUC__ */
